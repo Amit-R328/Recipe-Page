317ee7ecb66a4ad0f83024b1265229654c4c74a1
Amit Rozen <amitrozen2015@gmail.com>
2024-04-25 14:08:33 +0300

    Add files via upload

---
+++ src/app/nutrition-facts/nutrition-facts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nutrition-facts',
+  templateUrl: './nutrition-facts.component.html',
+  styleUrls: ['./nutrition-facts.component.sass']
+})
+export class NutritionFactsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
